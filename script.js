3b41cd1fa35d4579d09cdf57c10c55859baae0a5
aurianebgnl <auriane.bgnl@gmail.com>
2025-04-23 15:41:28 +0200

    retrait affichage secondes

---
+++ script.js
@@ -0,0 +1,30 @@
+<<<<<<< main
+=======
+function getDate () {
+    const now = new Date();
+
+    const options = { weekday: 'short', day: 'numeric' , month: 'long'};
+    const dateFormat = new Intl.DateTimeFormat('fr-FR', options).format(now);
+
+    return dateFormat
+}
+
+function getTime() {
+    const now = new Date();
+    const options = { hour: '2-digit', minute: '2-digit'};
+    const timeFormat = new Intl.DateTimeFormat('fr-FR', options).format(now);
+
+    return timeFormat
+}
+
+function updateDateTime() {
+    const dateDisplay = document.querySelector("#date");
+    const timeDisplay = document.querySelector("#time");
+
+    dateDisplay.innerHTML = getDate()
+    timeDisplay.innerHTML = getTime()
+}
+
+updateDateTime();
+const dateTimeInterval = setInterval(updateDateTime, 1000);
+>>>>>>> local
